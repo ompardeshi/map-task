603b03a3b62ec16c0b26bef42fd61c433b54b616
ompardeshi <venturesintothwild@gmail.com>
2023-01-05 01:01:57 +0530

    IMPLEMENTED MAP

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class= "bg-black h-screen m-5 p-5">
+    <Map class="h-full" ></Map>
   </div>
+
+
 </template>
+
+<script setup>
+</script>
---
+++ components/Map.vue
@@ -0,0 +1,25 @@
+<template>
+    <div ref="mapContainer"></div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+const maptoken = 'pk.eyJ1IjoiZW1pbGx5MTc2IiwiYSI6ImNsYzNjZHRiajN5enQzdnFuemNxNjdlbWYifQ.cG5N0l_CfwZDVgjIC3wGfQ'
+mapboxgl.accessToken = maptoken
+
+export default {
+data() {
+    return {
+    map: null
+    }
+},
+mounted() {
+    this.map = new mapboxgl.Map({
+    container: this.$refs.mapContainer,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    zoom: 1
+    })
+}
+}
+</script>
